830c82d81eda344aa5d94c7e9a29dbf31ece5dae
Miray Karabulut <mimikarabulut@gmail.com>
2023-05-10 13:51:28 -0500

    add babbel

--- index.js
+++ index.js
@@ -1,2 +1,18 @@
-var datetime = moment().startOf("day").fromNow();
-console.log(datetime);
+import moment from 'moment';
+// var moment = require('moment'); // require  
+// console.log(moment().format());
+
+// /* global moment */ 
+
+// var datetime = moment().startOf("day").fromNow();
+// console.log(datetime);
+
+console.log(moment().format("dddd"));
+console.log(moment().format("MMM Do YY"));
+console.log(moment().format("MMMM Do YYYY, h:mm:ss a"));
+
+console.log(moment().format());
+
+
+console.log(var odds = evens.map(v => v + 1);)
+var nums = evens.map((v, i) => v + i);
---
+++ webpack.config.js
@@ -0,0 +1,33 @@
+// webpack.config.js
+module.exports = {
+  mode: 'development',
+  entry: './index.js',
+  output: {
+    filename: 'main.js',
+    publicPath: 'dist'
+  }
+};
+
+module.exports = {
+  mode: 'development',
+  entry: './index.js',
+  output: {
+    filename: 'main.js',
+    publicPath: 'dist'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env']
+          }
+        }
+      }
+    ]
+  }
+};
+
